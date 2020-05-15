425998c15e7cdcc33f09f1f5ff7184325f1746c8
JohnEllefson <johnellefson@byui.edu>
2020-05-15 16:11:58 -0500

    Weather Site Preston Page Progress

---
+++ lesson4/js/preston-4.js
@@ -0,0 +1,14 @@
+// I had a hard time trying to display the date, but Daniel Schmitz in my learning group
+// meeting shared the solution that he had found which helped me tremendously with this part
+
+// Displays Date
+let day = new Date().getDate();
+const dayOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+document.getElementById("currentYear").textContent = new Date().getFullYear();
+document.getElementById("currentDate").innerHTML = dayOfWeek[new Date().getDay()] + ", " + (new Date().toLocaleString("en-GB",{month:'long', year:'numeric', day:'numeric'}))
+
+// Makes the hamburger button responsive
+const menubutton = document.querySelector('.response');
+const nav = document.querySelector('.nav-bar')
+
+menubutton.addEventListener('click', () => {nav.classList.toggle('responsive')}, false);
