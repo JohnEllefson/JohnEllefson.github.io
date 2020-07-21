b52b6bd0a87e47191cb439979de8b5f583b35f6c
JohnEllefson <johnellefson@byui.edu>
2020-07-21 06:10:12 -0500

    changes

--- ~Scoots/js/rental-info-api.js
+++ ~Scoots/js/rental-info-api.js
@@ -1,27 +1,35 @@
-//fetch(".data/rental-info.json")
-   // .then((response) => response.json())
-   // .then((jsObject) => {
+fetch("https://raw.githubusercontent.com/JohnEllefson/JohnEllefson.github.io/master/~Scoots/data/rental-info.json")
+    .then((response) => response.json())
+    .then((jsObject) => {
 
 
-
-
-        document.getElementById("max-persons1").textContent = 10;
-
-
-
+        //document.getElementById(`max-persons${1}`).textContent = 10;
         
+        //const rentalsList = jsonObject['rentals'];
+        let rentalNum = 0;
 
-        //const rentalsList = jsObject;
-        //let rentalNum = 0;
+
+
+
+        document.getElementById(`max-persons${1}`).textContent = rentals[0].maxPersons;
+
+
+
+
+
 
         //rentalsList.forEach(vehical => {
+            
+        //rentalsList.filter(rental => (rental.name == 'hondaMetro')).forEach(rental => {
 
-        //document.getElementById(`max-persons${rentalNum + 1}`).textContent = vehical.maxPersons;
+        //document.getElementById(`max-persons${1}`).textContent = maxPersons;
         //document.getElementById(`reservation-half-day${rentalNum}`).textContent = vehical.reservation.halfDay;
         //document.getElementById(`reservation-full-day${rentalNum}`).textContent = vehical.reservation.fullDay;
         //document.getElementById(`walkIn-half-day${rentalNum}`).textContent = vehical.walkIn.halfDay;
         //document.getElementById(`walkIn-half-day${rentalNum}`).textContent = vehical.walkIn.fullDay;
 
-        //rentalNum++
-        //});
-  //  });
+
+
+        rentalNum++
+    //});
+});
